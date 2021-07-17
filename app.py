cfb5a3b5a6e375b91d11d0eaf74a26333f4b0076
Dresselhouse <84082517+Dresselhouse@users.noreply.github.com>
2021-07-17 21:40:06 +0200

    wordcloud

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask.templating import render_template
 from flask.wrappers import Request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 from werkzeug.utils import redirect
 
+import matplotlib.pyplot as plt
+import nltk
+from wordloud import WordCloud
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bis_cb_speches_db_20_21.db'
@@ -28,6 +30,10 @@ def index():
     if request.method == 'POST':
         zug_name = request.form['name']
         speeches = Speeches.query.filter(Speeches.pdf.contains(zug_name)).order_by(Speeches.date).limit(10).all()
+        textt = ""
+        for speech in speeches:
+            textt.join(speech.pdf)        
+        wordcloud = WordCloud().generate(textt)
         return render_template('index.html', speeches = speeches)
         
 
