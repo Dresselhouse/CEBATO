55d09b87b88fbcd9e1fe992cf03abc54b6b51b6c
Dresselhouse <84082517+Dresselhouse@users.noreply.github.com>
2021-08-09 16:47:43 +0200

    cleanup

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
-from flask import Flask, url_for, request
+# Flask Web-Framework
+from flask import Flask, request
 from flask.templating import render_template
-from flask.wrappers import Request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
-from werkzeug.utils import redirect
 
+# Standard Imports
+from datetime import datetime, timedelta
 import os
 import glob
 from random import randint, random
 import math
 import random
 
+# Plots
 import matplotlib.pyplot as plt
+
+# Wordcloud
 from wordcloud import WordCloud
 
-# world map
+# World Map
 from pycountry_convert import country_name_to_country_alpha2
-from pycountry_convert import country_alpha2_to_country_name
 import pygal
 from pygal.style import DarkStyle
 
-#LDA
+#Natural Language Processing
 import spacy
 nlp = spacy.load('en_core_web_sm')
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
+
+################
+# Config-Limits 
+# (prevent server shutdowns. Can be set to high numbers when running local)
+#################
+
 kws_speeches_max = 200
 lda_speeches_max = 50
 
 
+
+# Initializing the app and databases
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bis_cb_speches_db_20_21.db'
 app.config['SQLALCHEMY_BINDS'] = {
@@ -47,6 +57,10 @@ app.config['SQLALCHEMY_BINDS'] = {
         "db2021": "sqlite:///bis_cb_speeches_db_21_clean.db"}
 db = SQLAlchemy(app)
 
+
+#####################################
+# Database Models for different years
+#####################################
 
 class Speeches2010(db.Model):
     __bind_key__ = "db2010"
@@ -125,7 +139,6 @@ class Speeches2015(db.Model):
 
     def __repr__(self):
         return '<Speech %r>' % self.id
-
 
 class Speeches2016(db.Model):
     __bind_key__ = "db2016"
@@ -206,13 +219,20 @@ class Speeches2021(db.Model):
         return '<Speech %r>' % self.id
 
 
+##########################
+# Custom defined functions
+##########################
+
 def random_with_N_digits(n):
+    # Used for randomized Image Paths to prevent browsers from showing old, cached plots
     range_start = 10**(n-1)
     range_end = (10**n)-1
     return randint(range_start, range_end)
 
 
 def pagination(speeches):
+
+    # Seperating the list of speeches from the search to chunks
 
     if len(speeches) > 25:
         speeches = speeches[0:25]
@@ -225,12 +245,15 @@ def pagination(speeches):
 
 
 def delete_old_wordclouds():
+    # Search for all old worldclouds and delete them
+
     all_wordclouds = glob.glob("static/images/wordcloud*")
     for path in all_wordclouds:
         os.remove(path)
 
 
 def create_new_wordCloud(speeches):
+    # Generates a new wordcloud from a list of speeches, returns the file path
 
     delete_old_wordclouds()
     textt = "empty empty empty results"
@@ -245,6 +268,8 @@ def create_new_wordCloud(speeches):
 
 
 def delete_old_timelines():
+    # Searches for all old timelines and deletes them
+
     all_timelines = glob.glob("static/images/timeline*")
     for path in all_timelines:
         os.remove(path)
@@ -254,7 +279,7 @@ def create_new_timeline(speeches, keyword):
     # delete old timelines
     delete_old_timelines()
     plt.clf()
-    # loop through speeches and get count of keyword
+    # Prepares the data for the plot
     keys = []
     values = []
     for speech in speeches:
@@ -262,18 +287,19 @@ def create_new_timeline(speeches, keyword):
         keys.append(datetime.fromtimestamp(speech.date))
         values.append(speech.pdf.upper().count(keyword.upper()))
 
+    # Plot creation and export
     random_path = random_with_N_digits(5)
     new_timeline_path = "static/images/timeline" + str(random_path) + ".png"
     plt.style.use("dark_background")
     plt.scatter(keys, values)
-
     plt.savefig(new_timeline_path, bbox_inches='tight')
 
     return new_timeline_path
 
 
 def prepare_map_data(speeches, keyword):
-    
+    # Counts the number of hits for every speech and every country
+
     data = {}
     data_alpha = {}
     for speech in speeches:
@@ -291,46 +317,53 @@ def prepare_map_data(speeches, keyword):
             data[speech.country][1] += 1
 
     for country in data:
+        # Try to convert the country name into the Alpha2-Code
+        # If it is successfull the country and its hit code gets added to the list
+        # This list gets later passed to the Pygal Package to create the world map
+        # The other list is used for the display of the top 3 countries
+
         try:
             cn_a2_code = country_name_to_country_alpha2(country).lower() 
             data_alpha[cn_a2_code] = data[country][1]
         except:
             cn_a2_code = 'Unknown'
-
-        
-            
+         
 
     return data, data_alpha
 
 
 def delete_old_worldmaps():
+    # Search for old worldmaps and delete them
+
     all_worldmaps = glob.glob("static/images/worldmap*")
     for path in all_worldmaps:
         os.remove(path)
 
 
 def create_new_worldmaps(speeches, keyword):
+
     delete_old_worldmaps()
+
     # clear matplot from previous data
     plt.clf()
 
+    # Call Data-Prep-Function and save the results
     dataDicts = prepare_map_data(speeches, keyword)
     data = dataDicts[1]
     top_countries = [(k, dataDicts[0][k]) for k in sorted(dataDicts[0], key=dataDicts[0].get, reverse=True)][0:3]
-    # empty map
-
-    # pygal map
+    
+    # Create Pygal Map
     worldmap = pygal.maps.world.World(style=DarkStyle)
     worldmap.show_legend = False
 
+    # Add the data to the map
     worldmap.title = 'Countries'
     worldmap.add('Frequencies', data)
 
+    # Export as vector image
     random_path = random_with_N_digits(5)
     new_worldmap_path = "static/images/worldmap" + str(random_path) + ".svg"
-    worldmap.render_to_file(new_worldmap_path)
-
-    # Get Top 3 countries    
+    worldmap.render_to_file(new_worldmap_path)    
 
     return new_worldmap_path, top_countries
     
@@ -386,6 +419,10 @@ def query_database_all_area(keyword, area, start, end):
     if not start > 1626904800 or end < 1610060400:
         speeches.extend(Speeches2021.query.filter(Speeches2021.pdf.contains(
             keyword)).filter(Speeches2021.date > start).filter(Speeches2021.date < end).order_by(Speeches2021.date).all())
+
+    # If too many speeches are returned, a random sample is taken
+    # The size of this sample is defined at the start of the script
+    # List size gets reduced to prevent server shutdowns due to (free) hosting limitations
 
     if len(speeches) > kws_speeches_max:
         speeches = random.sample(speeches, kws_speeches_max)
@@ -446,6 +483,10 @@ def query_database_area(keyword, area, start, end):
         speeches.extend(Speeches2021.query.filter(Speeches2021.pdf.contains(
             keyword)).filter(Speeches2021.date > start).filter(Speeches2021.date < end).filter(Speeches2021.country == area).order_by(Speeches2021.date).all())
 
+    # If too many speeches are returned, a random sample is taken
+    # The size of this sample is defined at the start of the script
+    # List size gets reduced to prevent server shutdowns due to (free) hosting limitations
+
     if len(speeches) > kws_speeches_max:
         speeches = random.sample(speeches, kws_speeches_max)
         
@@ -454,6 +495,8 @@ def query_database_area(keyword, area, start, end):
     return speeches
 
 def query_database(keyword=' ', area='all', start=(datetime.utcnow() - timedelta(days=30)).timestamp(), end=datetime.utcnow().timestamp()):
+
+    # Convert the inputs of the dates to date objects
 
     try:
         start = datetime.fromisoformat(start).timestamp()
@@ -465,7 +508,7 @@ def query_database(keyword=' ', area='all', start=(datetime.utcnow() - timedelta
     except:
         end = datetime.utcnow().timestamp()   
 
-
+    # Check for specified area to query, or all get queried
 
     if area == 'all':
         speeches = query_database_all_area(keyword, area, start, end)
@@ -477,65 +520,78 @@ def query_database(keyword=' ', area='all', start=(datetime.utcnow() - timedelta
 # Date filter
 @app.template_filter('datefromint')
 def format_date(int):
+    # Custom filter to format the date object
+    # Filter gets applied the jinja2-html template
+
     date = datetime.fromtimestamp(int)
     date_formatted = date.strftime('%d.%m.%Y')
     return date_formatted
 
 
+################
+# Keyword Search
+################
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        #############
-        # Suchanfrage
-        #############
+        ################
+        # Search Request
+        ################
+
+        # Search parameters get collected
         keyword = request.form['keyword']
         area = request.form['area']
         startdate = request.form['startdate']
         enddate = request.form['enddate']
 
+        # Database query and pagination
         speeches = query_database(keyword, area, startdate, enddate)
-
         speeches_list = pagination(speeches)
-        frequencies = create_new_timeline(speeches, keyword)
 
+
+        # Plots and Maps creation
         new_wordcloud_path = create_new_wordCloud(speeches)
         new_timeline_path = create_new_timeline(speeches, keyword)
         map_results = create_new_worldmaps(speeches, keyword)
         new_worldmap_path = map_results[0]
         top_countries = map_results[1]
-        
 
-        countries = [['Germany', 100, 20, 60], [
-            'France', 90, 33, 80], ['Netherlands', 80, 25, 66]]
-        img_paths = [new_wordcloud_path,
-                     new_timeline_path, new_worldmap_path]
+        img_paths = [new_wordcloud_path, new_timeline_path, new_worldmap_path]
 
-        return render_template('index.html', speeches_count=len(speeches), speeches=speeches_list, top_countries=top_countries, img_paths=img_paths, frequencies=frequencies)
+        return render_template('index.html', speeches_count=len(speeches), speeches=speeches_list, top_countries=top_countries, img_paths=img_paths)
 
     if request.method == 'GET':
         #####################
-        # Normaler Seitenload
+        # Regular Pageload
         #####################
+
+        # Results for default query (all areas, last 30 days)
         speeches = query_database()
         speeches_list = pagination(speeches)
 
+        # Wordcloud can be created without a keyword
         new_wordcloud_path = create_new_wordCloud(speeches)
 
-        frequencies = create_new_timeline(speeches, "1")
-
-        countries = [['Germany', 100, 20, 60], [
-            'France', 90, 33, 80], ['Netherlands', 80, 25, 66]]
+        # Wordcloud gets shown as placeholders for other plots
         img_paths = [new_wordcloud_path,
                      new_wordcloud_path, new_wordcloud_path]
 
-        return render_template('index.html', speeches_count=len(speeches), speeches=speeches_list, top_countries=countries, img_paths=img_paths, frequencies=frequencies)
+        return render_template('index.html', speeches_count=len(speeches), speeches=speeches_list, img_paths=img_paths)
 
     else:
+        # This case should never occur. Default query just in case
+
         speeches = query_database()
         return render_template('tour.html', speeches=speeches)
-        # return render_template('index.html')
+
+
+################
+# Topic Modeling
+################
 
 def delete_old_tm_results():
+    # Search for old scatterplots and delete them
     all_tm_results = glob.glob("static/images/tm_results*")
     for path in all_tm_results:
         os.remove(path)
@@ -592,9 +648,7 @@ def topic_modeling(speeches, num_comps):
         # topwords_and_percentages.append([topic_percentages[index], list(reversed(topics[index]))])
         topwords_and_percentages.append([percentage, word_string, index])
 
-    # Join word list to one string
-
-
+    # Results in descending order
     topwords_and_percentages.sort()
     topwords_and_percentages.reverse()
 
@@ -604,9 +658,9 @@ def topic_modeling(speeches, num_comps):
     plt.style.use("dark_background")
     plt.scatter(x, y, c=colors, alpha=0.9)
 
+    # Export as image
     random_path = random_with_N_digits(5)
     new_tm_results_path = "static/images/tm_results" + str(random_path) + ".png"
-
     plt.savefig(new_tm_results_path, bbox_inches='tight')
 
     return topwords_and_percentages, new_tm_results_path
@@ -615,9 +669,11 @@ def topic_modeling(speeches, num_comps):
 @app.route('/topicmodeling', methods=['POST', 'GET'])
 def topicmodeling():
     if request.method == 'POST':
-        ################
-        # Topic Modeling
-        ################
+        ############
+        # TM-Request
+        ############
+
+        # Get request parameters
         try:
             number_topics = int(request.form['numbertopics'])
         except:
@@ -626,9 +682,10 @@ def topicmodeling():
         startdate = request.form['startdate']
         enddate = request.form['enddate']
 
+        # Query database
         speeches = query_database(area=area, start=startdate, end=enddate)
 
-        #topics = [['eins', 'zwei', 'drei'], ['un', 'dos', 'tres']]
+        # Topic Modeling
         results = topic_modeling(speeches, num_comps=number_topics)
         topics = results[0]
         path = results[1]
@@ -636,9 +693,13 @@ def topicmodeling():
         return render_template('topicmodeling.html', speeches_count=len(speeches), topics=topics, path=path)
 
     else:
+        # Regular page load
         return render_template('topicmodeling.html')
 
 
+#############
+# Empty Pages
+#############
 @app.route('/tour', methods=['POST', 'GET'])
 def tour():
     if request.method == 'POST':
@@ -646,7 +707,6 @@ def tour():
 
     else:
         return render_template('tour.html')
-
 
 @app.route('/impressum', methods=['POST', 'GET'])
 def impressum():
@@ -657,5 +717,11 @@ def impressum():
         return render_template('impressum.html')
 
 
+#############
+# Main script
+#############
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    # Start the app
+    app.run()
